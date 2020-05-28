56d157464608668e6aa69e98e6b5e10de890a0a3
Sean Kaczanowski <seankaczanowski@gmail.com>
2020-05-28 14:12:51 +0000

    Cleaned up GUI, fixed bugs with incorrect input checks, cleaned up code.

--- main.py
+++ main.py
@@ -49,37 +49,38 @@ while i == 0: # Main Game Loop
 
   code = random.sample(colours, 5) # Code list sampled from colour options without replacement
   
-  gameboard = [] # List of results to show game progress
+  turns = 0 # Number of turns
+  gameboard = [['Gameboard'],['=========']] # List of results to show game progress
   guess = [] # Empty list for guess
   
   while code != guess: # Game loop until guess = code
+    turns += 1 # Turns counter
 
-    while j == 1: # Loop to control incorrect input for guess
+    k = 1
+    while k == 1: # Loop to control incorrect input for guess
 
       guess = (input('Code Guess: ')) # Guess
       guess = guess.upper() # Convert to uppercase
       guess = list(guess) # Convert to list
       
       if len(guess) != 5: # Check for correct length - 5
+        print(' ')
         print('Error: Type in 5 Colours')
         print('Try Again.')
+        print(' ')
       elif len(guess) > len(set(guess)): # Check for duplicates
+        print(' ')
         print('Error: Same Colour Used More Than Once.')
         print('Try Again.')
+        print(' ')
+      elif len(set(guess) & set(colours)) < 5: # Check if the correct colours are used
+        print(' ')
+        print('Error: Incorrect Colour Used.')
+        print('Try Again')
+        print(' ')
       else:
-        for item in guess: # check for incorrect colour
-          if item != 'R' or 'O' or 'Y' or 'G' or 'B' or 'K' or 'P' or 'W':
-           wrong_colour = 1 
-        if wrong_colour == 1:
-          print('Error: Incorrect Colour Used.')
-          print('Try Again')
-        else:
-          j == 0 # Correct - break the loop
-      
-    
-
-    gameboard.append(guess) # Add guess turn to gameboard
-
+        k = 0
+         
     result = [] # Result of turn to be added to gameboard
     for x, y in zip(code, guess): # Determine whether the guess is correct at each place in the code
       if x == y:
@@ -87,12 +88,21 @@ while i == 0: # Main Game Loop
       else:
         result.append('-')
 
-    set_guess = set(guess)
-    set_code = set(code)
-    common_colours = set_guess & set_code # Determines how many of the colours in the guess are in the code but not necessarily in the correct place
+    common_colours = set(guess) & set(code) # Determines how many of the colours in the guess are in the code but not necessarily in the correct place
+
+    guess.append(str(len(common_colours))) # Add how many correct colours guessed but not necessarily in code
+    gameboard.append(guess) # Add guess turn plus common colours to gameboard
   
     print(' ')
     for i in range(len(gameboard)): # Gameboard printing
       print(' '.join(gameboard[i]))
-    print(' '.join(result) + ' ' + str(len(common_colours))) # Gameboard result showing the correct/incorrect colour guesses and the number of colours that were guessed that are in the code
-    print('')
+    print(' '.join(result)) # Gameboard result showing the correct/incorrect colour guesses and the number of colours that were guessed that are in the code
+    
+
+    if guess == code: # Win scenario
+      print(' ')
+      print('Correct! You Win!')
+      print('Turns: ' + str(turns))
+      print(' ')
+    else:
+      print(' ')
