08d2cc84adc5566e4da10d2ecbc84e6d6f5f0e63
Sean Kaczanowski <seankaczanowski@gmail.com>
2020-05-28 11:26:15 +0000

    Added correct input check.

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ Simple Code Guessing Game
 - 8 Colours to Choose From
 '''
 
-
-import time # For small delays
 import random # For word selection
 
 i = 0
@@ -53,12 +51,34 @@ while i == 0: # Main Game Loop
   
   gameboard = [] # List of results to show game progress
   guess = [] # Empty list for guess
+  
   while code != guess: # Game loop until guess = code
-    guess = (input('Code Guess: ')) # Guess string
-    guess = guess.upper() # Convert to uppercase
-    guess = list(guess) # Convert to list
 
-    gameboard.append(guess)
+    while j == 1: # Loop to control incorrect input for guess
+
+      guess = (input('Code Guess: ')) # Guess
+      guess = guess.upper() # Convert to uppercase
+      guess = list(guess) # Convert to list
+      
+      if len(guess) != 5: # Check for correct length - 5
+        print('Error: Type in 5 Colours')
+        print('Try Again.')
+      elif len(guess) > len(set(guess)): # Check for duplicates
+        print('Error: Same Colour Used More Than Once.')
+        print('Try Again.')
+      else:
+        for item in guess: # check for incorrect colour
+          if item != 'R' or 'O' or 'Y' or 'G' or 'B' or 'K' or 'P' or 'W':
+           wrong_colour = 1 
+        if wrong_colour == 1:
+          print('Error: Incorrect Colour Used.')
+          print('Try Again')
+        else:
+          j == 0 # Correct - break the loop
+      
+    
+
+    gameboard.append(guess) # Add guess turn to gameboard
 
     result = [] # Result of turn to be added to gameboard
     for x, y in zip(code, guess): # Determine whether the guess is correct at each place in the code
@@ -71,8 +91,6 @@ while i == 0: # Main Game Loop
     set_code = set(code)
     common_colours = set_guess & set_code # Determines how many of the colours in the guess are in the code but not necessarily in the correct place
   
-    
-    
     print(' ')
     for i in range(len(gameboard)): # Gameboard printing
       print(' '.join(gameboard[i]))
